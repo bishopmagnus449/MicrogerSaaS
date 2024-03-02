9f25643b0e22cd168fdaf8f1b3a2dc2f74759322
realSamy <imhoodaji@gmail.com>
2024-03-03 03:13:00 +0330

    Install packages without confirmation

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install ca-certificates curl git
+sudo apt-get install -y ca-certificates curl git
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -12,8 +12,10 @@ $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-git clone https://github.com/realSamy/MicrogerSaaS && cd MicrogerSaaS
+git clone https://github.com/realSamy/MicrogerSaaS && cd MicrogerSaaS || exit 1
 
 sudo docker compose up -d
+
+chmod +x ./print_ip.sh
