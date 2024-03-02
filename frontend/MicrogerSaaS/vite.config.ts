41468ee90011607f2ed4d5defa8413da8fa0975f
realSamy <imhoodaji@gmail.com>
2024-03-03 00:31:53 +0330

    Add local proxy options

--- frontend/MicrogerSaaS/vite.config.ts
+++ frontend/MicrogerSaaS/vite.config.ts
@@ -1,19 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
-  server: {
-    port: 8080,
-  }
+    server: {
+        port: 8080,
+        proxy: {
+            "^/(api|static)/": 'http://localhost:8000',
+            "^/(ws)/": 'ws://localhost:8000'
+        }
+    },
+    build: {
+        assetsDir: 'app',
+    },
+    base: process.env.NODE_ENV === 'development' ? '/' : '/static/',
 })
