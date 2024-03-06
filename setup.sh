2148a7710e1cfff95fcbd9312f4ec874a2c6f77b
realSamy <imhoodaji@gmail.com>
2024-03-06 09:40:41 +0330

    Rollback last commit

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export DEBIAN_FRONTEND=noninteractive
-export GNUTLS_CPUID_OVERRIDE=0x1
-
 if ! command -v docker compose >/dev/null 2>&1; then
   sudo apt-get update
   sudo apt-get install -y ca-certificates curl git
