97d52249a2429d08d641aed3ca0ae2382f527635
realSamy <imhoodaji@gmail.com>
2024-03-13 05:42:41 +0330

    Bug fix

--- setup.sh
+++ setup.sh
@@ -8,9 +8,9 @@ if ! command -v docker >/dev/null 2>&1; then
   sudo chmod a+r /etc/apt/keyrings/docker.asc
 
   echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
 
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
